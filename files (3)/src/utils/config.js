d0ae247a4190fff8f484f9ec12cec78328de70e7
dodd92929-dev <dodd92929@gmail.com>
2025-08-07 02:10:03 +0800

    Delete files (3) directory

--- files (3)/src/utils/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  BIDDERS_ROLE: process.env.BIDDERS_ROLE || 'Bidders',
-  LOG_CHANNEL_ID: process.env.LOG_CHANNEL_ID,
-  BID_RATE_LIMIT: 5000 // ms between bids per user per auction
-};
